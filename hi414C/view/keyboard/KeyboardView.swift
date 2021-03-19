a32ddcecb8168067442496fc6621d2e1b49b9fce
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-03-19 19:24:24 +0100

    Sorted by name

---
+++ hi414C/view/keyboard/KeyboardView.swift
@@ -0,0 +1,20 @@
+//
+//  KeyboardView.swift
+//  hi414C
+//
+//  Created by Darius Kryszczuk on 19.03.2021.
+//
+
+import SwiftUI
+
+struct KeyboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KeyboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        KeyboardView()
+    }
+}
---
+++ hi414C/view/terminal/TerminalCommandPrompt.swift
@@ -0,0 +1,21 @@
+//
+//  TerminalCommandPrompt.swift
+//  hi414C
+//
+//  Created by Darius Kryszczuk on 19.03.2021.
+//
+
+import SwiftUI
+
+// TODO 414C > |
+struct TerminalCommandPrompt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TerminalCommandPrompt_Previews: PreviewProvider {
+    static var previews: some View {
+        TerminalCommandPrompt()
+    }
+}
