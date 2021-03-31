5a80af86633cf28da57ea4fda90f9c37988d05a8
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-03-31 15:31:03 +0200

    Added DEL and ACK ANSIRegular figlets

---
+++ hi414C/view/keyboard/ascii/ASCIIKeyboardKeyView.swift
@@ -0,0 +1,20 @@
+//
+//  ASCIIKeyboardKeyView.swift
+//  hi414C
+//
+//  Created by Darius Kryszczuk on 31.03.2021.
+//
+
+import SwiftUI
+
+struct ASCIIKeyboardKeyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ASCIIKeyboardKeyView_Previews: PreviewProvider {
+    static var previews: some View {
+        ASCIIKeyboardKeyView()
+    }
+}
