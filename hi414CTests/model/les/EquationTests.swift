69f158e9f72b7c0cc1aefc2d1301ed5332bf4776
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-04-13 17:33:08 +0200

    Added preview of SUB

--- hi414CTests/model/les/EquationTests.swift
+++ hi414CTests/model/les/EquationTests.swift
@@ -52,7 +52,7 @@ class EquationTests: XCTestCase {
     }
     
     func test_of_SHL_equations() {
-        print(ADD() => 65)
+        print(SUB() => 65)
         //let result = 65 << 1
         
         /*
