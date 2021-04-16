1b18de91ae6a5edfe9053675f79c7ec3cd1e8d0c
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-04-16 16:52:33 +0200

    Terminal Grid but useless

---
+++ hi414C/view/terminal/TerminalGrid.swift
@@ -0,0 +1,20 @@
+//
+//  TerminalGrid.swift
+//  hi414C
+//
+//  Created by Darius Kryszczuk on 16.04.2021.
+//
+
+import SwiftUI
+
+struct TerminalGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TerminalGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        TerminalGrid()
+    }
+}
