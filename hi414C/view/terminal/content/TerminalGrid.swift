539f5c9059be5639178c9ed313852b2a4c77d275
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-05-06 13:55:35 +0200

    Added easyOut animation to background

--- hi414C/view/terminal/content/TerminalGrid.swift
+++ hi414C/view/terminal/content/TerminalGrid.swift
@@ -166,7 +166,7 @@ struct TerminalGrid: View {
     func HelpBackground() -> some View {
         themeVM.terminal.help.background
             .frame(width: UIScreen.main.bounds.width + 100, height: UIScreen.main.bounds.height + 100)
-            .transition(.circular)
+            .transition(AnyTransition.circular.animation(.easeOut.speed(1)))
     }
     
     func TerminalArt(_ arts: [ASCIIPrintable]) -> some View {
