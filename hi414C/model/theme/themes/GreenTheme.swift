0b83be631cb79ba179b79e739300b1f23419b323
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-05-14 18:10:08 +0200

    Added green theme

---
+++ hi414C/model/theme/themes/GreenTheme.swift
@@ -0,0 +1,23 @@
+//
+//  GreenTheme.swift
+//  hi414C
+//
+//  Created by Darius Kryszczuk on 14.05.2021.
+//  palette: https://colorhunt.co/palette/264502, https://colorhunt.co/palette/281939
+
+import SwiftUI
+
+class GreenTheme: Theme {
+    
+    override init(
+        font: FontTheme = FontTheme(),
+        color: ColorTheme = ColorTheme(
+            primary: (Color("PrimaryGreen"), Color("SecondaryGreen")),
+            secondary: (Color("SecondaryGreen"), Color("WhiteGreen")),
+            tertiary: (Color("TertiaryGreen"), Color("WhiteGreen")),
+            background: .black
+        )) {
+        super.init(font: font, color: color)
+    }
+    
+}
--- hi414C/viewmodel/ThemeViewModel.swift
+++ hi414C/viewmodel/ThemeViewModel.swift
@@ -15,7 +15,7 @@ class ThemeViewModel: ObservableObject {
     var terminal: TerminalTheme { theme.terminal }
     
     init(theme: Themable = OrangeTheme()) {
-        self.theme = OrangeTheme()
+        self.theme = GreenTheme()
     }
     
     func font(_ font: FontTheme) {
