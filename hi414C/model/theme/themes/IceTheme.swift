272cf9ca7c8050abc2ef8cbaefa053aab36a0a92
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-05-16 23:10:21 +0200

    Added IceTheme

---
+++ hi414C/model/theme/themes/IceTheme.swift
@@ -0,0 +1,22 @@
+//
+//  IceTheme.swift
+//  hi414C
+//
+//  Created by Darius Kryszczuk on 16.05.2021.
+//  palette: https://colorhunt.co/palette/180289
+
+import SwiftUI
+
+class IceTheme: Theme {
+
+    override init(
+        font: FontTheme = FontTheme(),
+        color: ColorTheme = ColorTheme(
+            primary: (Color("PrimaryIce"), Color("SecondaryIce")),
+            secondary: (Color("SecondaryIce"), Color("WhiteIce")),
+            tertiary: (Color("TertiaryIce"), Color("WhiteIce")),
+            background: Color("SecondaryIce")
+        )) {
+        super.init(font: font, color: color)
+    }
+}
