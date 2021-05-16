6976a810c1eceac1b607e79294b8ac3f4e6cf496
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-05-16 23:14:25 +0200

    Added BananaSkyTheme

---
+++ hi414C/model/theme/themes/BananaSkyTheme.swift
@@ -0,0 +1,22 @@
+//
+//  BananaSkyTheme.swift
+//  hi414C
+//
+//  Created by Darius Kryszczuk on 16.05.2021.
+//  palette: https://colorhunt.co/palette/180404
+
+import SwiftUI
+
+class BananaSkyTheme: Theme {
+
+    override init(
+        font: FontTheme = FontTheme(),
+        color: ColorTheme = ColorTheme(
+            primary: (Color("PrimaryBananaSky"), Color("SecondaryBananaSky")),
+            secondary: (Color("SecondaryBananaSky"), Color("WhiteBananaSky")),
+            tertiary: (Color("TertiaryBananaSky"), Color("WhiteBananaSky")),
+            background: Color("SecondaryBananaSky")
+        )) {
+        super.init(font: font, color: color)
+    }
+}
