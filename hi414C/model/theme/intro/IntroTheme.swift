b6f256eea48cbfb3d137cca28fcb65c7cf752edc
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-05-20 09:05:06 +0200

    Added tap to start text

---
+++ hi414C/model/theme/intro/IntroTheme.swift
@@ -0,0 +1,10 @@
+//
+//  IntroTheme.swift
+//  hi414C
+//
+//  Created by Darius Kryszczuk on 20.05.2021.
+//
+
+struct IntroTheme {
+    var cursor: TerminalTheme.CommandLine.Cursor
+}
--- hi414C/view/intro/IntroScreen.swift
+++ hi414C/view/intro/IntroScreen.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct IntroScreen: View {
     @EnvironmentObject var themeVM: ThemeViewModel
+    @EnvironmentObject var asciiVM: ASCIIViewModel
     @State var isCursor = true
+    @State var textOpacity: Double = 0
     
     var body: some View {
         GeometryReader { metrics in
@@ -30,16 +32,29 @@ struct IntroScreen: View {
                     }
                 }
             } else {
-                HStack {
-                    FigletView("414C", theme: FigletTheme(
-                        typeface: .ansi(.shadow),
-                        view: ViewTheme(
-                            font: FontProps(.terminus, 23),
-                            color: Color("PrimaryOrange")
-                        ),
-                        animations: [.print(speed: 0.5, delay: 0), .bloom(speed: 1, color: Color("PrimaryOrange"))]
-                    ))
-                    .bloom(color: Color("PrimaryOrange"))
+                VStack {
+                    HStack {
+                        FigletView("414C", theme: FigletTheme(
+                            typeface: .ansi(.shadow),
+                            view: ViewTheme(
+                                font: FontProps(.terminus, 23),
+                                color: Color("PrimaryOrange")
+                            ),
+                            animations: [.print(speed: 0.4, delay: 0.3), .bloom(speed: 0.3, color: Color("PrimaryOrange"))]
+                        ))
+                        .bloom(color: Color("PrimaryOrange"))
+                    }
+                    Spacer()
+                    Text(asciiVM.symbols.contains(.H) ? "tap to continue" : "tap to start")
+                        .font(Font.of(props: FontProps(.proggyTiny, 32)))
+                        .foregroundColor(Color("PrimaryOrange"))
+                        .opacity(textOpacity)
+                        .bloom(color: Color("PrimaryOrange"))
+                        .onAppear {
+                            withAnimation(Animation.easeIn(duration: 2).delay(3.3)) {
+                                self.textOpacity = 0.8
+                            }
+                        }
                 }
                 .padding(30)
                 .frame(width: metrics.size.width, height: metrics.size.height, alignment: .top)
