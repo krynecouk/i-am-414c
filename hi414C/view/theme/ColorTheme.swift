352f28076d16f9d9c897f340009ffc49430c67f4
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-06-30 18:42:21 +0200

    Removed Color.random()

--- hi414C/view/theme/ColorTheme.swift
+++ hi414C/view/theme/ColorTheme.swift
@@ -20,12 +20,3 @@ extension Color {
         Color("Tertiary\(name)")
     }
 }
-
-extension Color {
-    static func random() -> Color {
-        let r = Double.random(in: 0 ... 1)
-        let g = Double.random(in: 0 ... 1)
-        let b = Double.random(in: 0 ... 1)
-        return Color(red: r, green: g, blue: b)
-    }
-}
