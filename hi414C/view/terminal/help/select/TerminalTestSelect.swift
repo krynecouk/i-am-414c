ab936e1864de7a22547758fe363a24047a558b35
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-07-29 00:39:10 +0200

    Finished with terminal help select

--- hi414C/view/terminal/help/select/TerminalTestSelect.swift
+++ hi414C/view/terminal/help/select/TerminalTestSelect.swift
@@ -12,64 +12,109 @@ struct TerminalTestSelect: View {
     @EnvironmentObject var themeVM: ThemeViewModel
     
     @State var frame: CGRect = CGRect()
-    @State var portrait: Bool = true
-    @State var large: Bool = true
+    @State var landscape: Bool = false
+    @State var tablet: Bool = false
     
     var body: some View {
         Group {
-            VStack(alignment: .leading) {
-                if portrait {
-                    Buttons()
-                }
-                Slider()
+            if tablet {
+                TabletLayout()
+            } else {
+                MobileLayout()
             }
         }
         .background(GeometryReader { metrics in
             Color.clear
                 .onAppear {
                     self.frame = metrics.frame(in: .global)
-                    self.portrait = self.frame.maxY > 400
-                    print(frame.size)
+                    self.landscape = self.frame.maxY < 400
+                    self.tablet = self.frame.maxX > 700 && self.frame.maxY > 700
+                    print(frame.maxX, frame.maxY)
                 }
                 .onChange(of: metrics.size) { size in
                     self.frame = metrics.frame(in: .global)
-                    self.portrait = self.frame.maxY > 400
-                    print(frame.size)
+                    self.landscape = self.frame.maxY < 400
+                    self.tablet = self.frame.maxX > 700 && self.frame.maxY > 700
+                    print(frame.maxX, frame.maxY)
                 }
         })
-        .padding(portrait ? 20 : 10)
-        .padding(.bottom, portrait ? 69 : 62)
+        .padding(landscape ? 10 : 20)
+        .padding(.bottom, landscape ? 62 : 69)
         .background(themeVM.terminal.hli.select.background.active.edgesIgnoringSafeArea(.all))
-        .transition(AnyTransition.move(edge: .bottom).combined(with: .offset(y: SegueViewModel.header.height)))
+        //.transition(AnyTransition.move(edge: .bottom).combined(with: .offset(y: SegueViewModel.header.height)))
     }
     
-    func Buttons() -> some View {
+    func TabletLayout() -> some View {
         HStack {
-            HelpButton("-") {
-                helpVM.decrement()
+            TabletButtons()
+            Slider()
+        }
+    }
+    
+    func MobileLayout() -> some View {
+        VStack(alignment: .leading) {
+            if !landscape {
+                MobileButtons()
             }
+            Slider()
+        }
+    }
+    
+    func TabletButtons() -> some View {
+        HStack {
+            Zero()
+                .padding(.trailing, 10)
+            Radix()
+        }
+        .padding(20)
+        .background(RoundedRectangle(cornerRadius: 15.0)
+                        .fill(Color("TertiaryOrange")))
+        .padding(.trailing, 10)
+    }
+    
+    func MobileButtons() -> some View {
+        HStack {
+            Minus()
             Spacer()
-            HelpButton("+") {
-                helpVM.increment()
-            }
+            Plus()
             Spacer()
-            HelpRadioButton("0", active: helpVM.equation.result != 0, sound: (.delete, .delete)) {
-                helpVM.resetToZero()
-            }
-            .withDisabledSound(if: helpVM.equation.result == 0)
+            Zero()
             Spacer()
-            HelpButton(helpVM.radix == .bin ? "hex" : "bin") {
-                helpVM.radix(of: helpVM.radix == .bin ? .hex : .bin)
-            }
+            Radix()
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 15.0)
                         .fill(Color("TertiaryOrange")))
     }
     
+    func Minus() -> some View {
+        HelpButton("-") {
+            helpVM.decrement()
+        }
+    }
+    
+    func Plus() -> some View {
+        HelpButton("+") {
+            helpVM.increment()
+        }
+    }
+    
+    func Zero() -> some View {
+        HelpRadioButton("0", active: helpVM.equation.result != 0, sound: (.delete, .delete)) {
+            helpVM.resetToZero()
+        }
+        .withDisabledSound(if: helpVM.equation.result == 0)
+    }
+    
+    func Radix() -> some View {
+        HelpButton(helpVM.radix == .bin ? "hex" : "bin") {
+            helpVM.radix(of: helpVM.radix == .bin ? .hex : .bin)
+        }
+    }
+    
     func Slider() -> some View {
         HStack {
-            if !portrait {
+            if landscape {
                 HelpButton("-", size: (10, 10), padding: 10, color: Color("WhiteOrange"), withBackground: false) { // TODO
                     helpVM.decrement()
                 }
@@ -77,6 +122,10 @@ struct TerminalTestSelect: View {
                 .padding([.leading, .trailing], 5)
             }
 
+            if tablet {
+                Minus()
+            }
+            
             SwiftUI.Slider(
                 value: Binding(get: {
                     Double(helpVM.equation.result)
@@ -87,19 +136,22 @@ struct TerminalTestSelect: View {
                 step: 1
             )
             .accentColor(Color("PrimaryOrange")) // TODO!!!
-            if !portrait {
+            if landscape {
                 HelpButton("+", size: (10, 10), padding: 10, color: Color("WhiteOrange"), withBackground: false) { // TODO
                     helpVM.increment()
                 }
                 .offset(y: 3.5)
                 .padding([.leading, .trailing], 5)
             }
+            if tablet {
+                Plus()
+            }
         }
-        .padding(portrait ? 20 : 5)
-        .background(portrait ? RoundedRectangle(cornerRadius: 15.0)
-                        .fill(Color("TertiaryOrange")) : RoundedRectangle(cornerRadius: 15.0)
-                        .fill(Color.clear))
-        .padding(.top, portrait ? 5 : 0)
+        .padding(landscape ? 5 : 20)
+        .background(landscape ? RoundedRectangle(cornerRadius: 15.0)
+                        .fill(Color.clear) : RoundedRectangle(cornerRadius: 15.0)
+                        .fill(Color("TertiaryOrange")))
+        .padding(.top, landscape || tablet ? 0 : 5)
     }
 }
 
